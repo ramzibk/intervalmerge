f2972e4f40c2694e7294c6cae76edd985f898976
Ramzi Ben Khedhiri <bk.ramzi@gmail.com>
2020-12-01 23:58:34 +0100

    commit interval tests

---
+++ src/test/java/com/rbk/intervalmerge/IntervalTest.java
@@ -0,0 +1,32 @@
+package com.rbk.intervalmerge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntervalTest {
+
+    @Test
+    public void testEquals(){
+        Interval interval = new Interval(45, 13);
+        assertEquals(interval,new Interval(45, 13));
+        assertNotEquals(interval, equals(new Interval(20, 8)));
+    }
+
+
+    @Test
+    void testInterval_from_String(){
+        Interval interval;
+        interval = new Interval("[12,5]");
+        assertTrue(interval.equals(new Interval(12,5)));
+
+        assertTrue(new Interval(" [ 12 , 5]  ").equals(new Interval(12,5)));
+        assertTrue(interval.equals(new Interval(12,5)));
+
+        assertTrue(new Interval(" [ 12 , 9999]  ").equals(new Interval(12,9999)));
+        assertTrue(interval.equals(new Interval(12,5)));
+
+        assertTrue(new Interval(" [ 12 , 9999]  ").equals(new Interval(12,9999)));
+        assertTrue(interval.equals(new Interval(12,5)));
+    }
+}
